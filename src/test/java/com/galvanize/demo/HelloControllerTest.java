63d0e236459901b220e7ed8d8df99326d3b3c479
manzaloros <zacharymansellcello@gmail.com>
2020-12-07 17:11:21 -0600

    Add working hello world test file

---
+++ src/test/java/com/galvanize/demo/HelloControllerTest.java
@@ -0,0 +1,26 @@
+package com.galvanize.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+// We always want this particular import for these tests:
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+// Tell Spring to load the minimal config just to load this one controller
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void testHomepage() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/");
+
+        this.mvc.perform(request).andExpect(content().string("Hello from Spring"));
+    }
+}
