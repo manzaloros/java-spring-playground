36f54de40e627b8e5579555e1b2c8c3c377ed5ae
manzaloros <zacharymansellcello@gmail.com>
2020-12-08 13:56:16 -0600

    Refactor Arrays.asList in indefinite args

--- src/test/java/com/galvanize/demo/MathServiceTest.java
+++ src/test/java/com/galvanize/demo/MathServiceTest.java
@@ -3,6 +3,7 @@ package com.galvanize.demo;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,10 +19,7 @@ public class MathServiceTest {
     }
 
     void additionIndefiniteArgs() {
-        List<String> numbers = new ArrayList<String>();
-        numbers.add("5");
-        numbers.add("4");
-        numbers.add("3");
+        List<String> numbers = Arrays.asList("5", "4", "3");
         assertEquals("5 + 4 + 3 = 12", mathServiceIndefinite.add());
     }
 }
