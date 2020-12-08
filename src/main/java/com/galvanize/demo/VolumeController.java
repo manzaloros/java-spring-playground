20699e117401c1d315792dc0103027b030ef2d5a
manzaloros <zacharymansellcello@gmail.com>
2020-12-08 15:02:27 -0600

    Add volume controller and tests

---
+++ src/main/java/com/galvanize/demo/VolumeController.java
@@ -0,0 +1,43 @@
+package com.galvanize.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/math/volume/{l}/{w}/{h}")
+public class VolumeController {
+
+    @GetMapping("")
+    public String getCalc(@PathVariable int l,
+                            @PathVariable  int w,
+                            @PathVariable int h) {
+        return Integer.toString(l * w * h);
+    }
+
+    @PostMapping("")
+    public String postCalc(@PathVariable int l,
+                            @PathVariable  int w,
+                            @PathVariable int h) {
+        return Integer.toString(l * w * h);
+    }
+
+    @PatchMapping("")
+    public String patchCalc(@PathVariable int l,
+                            @PathVariable  int w,
+                            @PathVariable int h) {
+        return Integer.toString(l * w * h);
+    }
+
+    @PutMapping("")
+    public String putCalc(@PathVariable int l,
+                            @PathVariable  int w,
+                            @PathVariable int h) {
+        return Integer.toString(l * w * h);
+    }
+
+    @DeleteMapping("")
+    public String deleteCalc(@PathVariable int l,
+                            @PathVariable  int w,
+                            @PathVariable int h) {
+        return Integer.toString(l * w * h);
+    }
+}
---
+++ src/test/java/com/galvanize/demo/VolumeControllerTest.java
@@ -0,0 +1,40 @@
+package com.galvanize.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class VolumeControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    void testVolume() throws Exception {
+        this.mvc.perform(get("/math/volume/3/4/5"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("60"));
+
+        this.mvc.perform(post("/math/volume/3/4/5"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("60"));
+
+        this.mvc.perform(patch("/math/volume/3/4/5"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("60"));
+
+        this.mvc.perform(put("/math/volume/3/4/5"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("60"));
+
+        this.mvc.perform(delete("/math/volume/3/4/5"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("60"));
+    }
+}
