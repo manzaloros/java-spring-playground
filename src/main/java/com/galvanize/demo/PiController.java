bef15576458935c4297c6ad454e2c84db1177d69
manzaloros <zacharymansellcello@gmail.com>
2020-12-08 11:02:51 -0600

    Add working operation tests on math calculate controller

--- src/main/java/com/galvanize/demo/PiController.java
+++ src/main/java/com/galvanize/demo/PiController.java
@@ -1,14 +1,48 @@
 package com.galvanize.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
+import static java.lang.Integer.parseInt;
+
 @RestController
+@RequestMapping("/math")
 public class PiController {
 
-    @GetMapping("/math/pi")
+    @GetMapping("/pi")
     public String returnPi() {
         return "3.141592653589793";
+    }
+
+    @GetMapping("/calculate")
+    public String math(@RequestParam Map map) {
+        int x = parseInt((String) map.get("x"));
+        int y = parseInt((String) map.get("y"));
+        int result;
+        if (map.containsKey("operation")) {
+            String operation = (String) map.get("operation");
+            System.out.println("op:" + operation);
+            switch (operation) {
+                case "add":
+                    result = x + y;
+                    return x + " + " + y + " = " + result;
+                case "multiply":
+                    result = x * y;
+                    return x + " * " + y + " = " + result;
+                case "subtract":
+                    result = x - y;
+                    return x + " - " + y + " = " + result;
+                case "divide":
+                    result = x / y;
+                    return x + " / " + y + " = " + result;
+            }
+        }
+        result = x + y;
+        return x + " + " + y + " = " + result;
     }
 
 }
--- src/test/java/com/galvanize/demo/PiControllerTest.java
+++ src/test/java/com/galvanize/demo/PiControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -18,10 +19,35 @@ public class PiControllerTest {
 
     @Test
     public void testPi() throws Exception {
-        RequestBuilder request = MockMvcRequestBuilders.get("/math/pi");
+        RequestBuilder request = get("/math/pi");
 
         this.mvc.perform(request)
                 .andExpect(status().isOk())
                 .andExpect(content().string("3.141592653589793"));
+    }
+
+    @Test
+    public void testMathQueryStrings() throws Exception {
+
+        this.mvc.perform(get("/math/calculate?operation=add&x=4&y=6"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 + 6 = 10"));
+
+        this.mvc.perform(get("/math/calculate?operation=multiply&x=4&y=6"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 * 6 = 24"));
+
+        this.mvc.perform(get("/math/calculate?operation=subtract&x=4&y=6"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 - 6 = -2"));
+
+        this.mvc.perform(get("/math/calculate?operation=divide&x=30&y=5"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("30 / 5 = 6"));
+
+        this.mvc.perform(get("/math/calculate?x=30&y=5"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("30 + 5 = 35"));
+
     }
 }
