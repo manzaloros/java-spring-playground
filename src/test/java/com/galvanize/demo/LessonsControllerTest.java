934871e811919d4096cecff4894598a550a53d4b
zacharymansell <zach.mansell@galvanize.com>
2020-12-10 11:00:10 -0600

    Add controller patch test

--- src/test/java/com/galvanize/demo/LessonsControllerTest.java
+++ src/test/java/com/galvanize/demo/LessonsControllerTest.java
@@ -15,8 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.IsInstanceOf.instanceOf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -57,6 +56,24 @@ public class LessonsControllerTest {
         this.mvc.perform(request)
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].id",
+                        equalTo(lesson.getId().intValue()) ));
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testPatch() throws Exception {
+        Lesson lesson = new Lesson();
+        lesson.setTitle("Unpatched_Title!");
+        repository.save(lesson);
+
+        MockHttpServletRequestBuilder request = patch("/lessons/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"title\": \"Test_Patch_Title\"}");
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id",
                         equalTo(lesson.getId().intValue()) ));
     }
 }
