7459c8238e3e7b119ec5c3e9b7541c05fa6f7e5e
zacharymansell <zach.mansell@galvanize.com>
2020-12-10 10:31:48 -0600

    Add db spring boot tests

---
+++ src/test/java/com/galvanize/demo/LessonsControllerTest.java
@@ -0,0 +1,62 @@
+package com.galvanize.demo;
+
+import com.galvanize.demo.Lesson;
+import com.galvanize.demo.LessonRepository;
+import com.galvanize.demo.LessonsController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LessonsControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    LessonRepository repository;
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testCreate() throws Exception {
+        MockHttpServletRequestBuilder request = post("/lessons")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"title\": \"Test_Title\"}");
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", instanceOf(Number.class)));
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testList() throws Exception {
+        Lesson lesson = new Lesson();
+        lesson.setTitle("Test_Title");
+        repository.save(lesson);
+
+        MockHttpServletRequestBuilder request = get("/lessons")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id",
+                        equalTo(lesson.getId().intValue()) ));
+    }
+}
